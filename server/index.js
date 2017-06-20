fb26edc11a9e01088c188065d045bc1cadb86853
= <webdev.jameslemire@gmail.com>
2017-06-20 10:16:10 -0600

    JL solution - Solution

--- server/index.js
+++ server/index.js
@@ -1,17 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+//
+const session = require('express-session');
+//
 const mc = require( __dirname + '/controllers/messages_controller');
+
+//
+const fc = require( `${__dirname}/controllers/filter_controller` );
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( __dirname + '/../public/build' ) );
+//
+app.use( session({
+  secret: '@nyth!ng y0u w@nt',
+  resave: false,
+  saveUninitialized: false
+}));
+//
 
-const baseURL = "/api/messages";
-app.post( baseURL, mc.create );
-app.get( baseURL, mc.read );
-app.put( `${baseURL}/:id`, mc.update );
-app.delete( `${baseURL}/:id`, mc.delete );
+app.use( ( req, res, next ) => {
+  const { session, method } = req;
+  if ( !session.user ) {
+    session.user = {
+      messages: []
+    };
+  }
+
+  if ( method === "POST" ) {
+    fc.filter( req, res, next );
+  } else {
+    next();
+  }
+} );
+
+
+const messagesBaseUrl = "/api/messages";
+app.post( messagesBaseUrl, mc.create );
+app.get( messagesBaseUrl, mc.read );
+app.put( `${messagesBaseUrl}`, mc.update );
+app.delete( `${messagesBaseUrl}`, mc.delete );
+
+app.get( `${messagesBaseUrl}/history`, mc.history );
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
